6a25c3e5ed50d0f7400e2b655d1d396f52de7209
m_srinivas <cnu.prince8@gmail.com>
2023-01-20 11:17:34 +0530

    product list

--- react-app/src/Components/products.js
+++ react-app/src/Components/products.js
@@ -4,11 +4,27 @@ import Card from 'react-bootstrap/Card';
 
 class Products extends Component {
     state = {
-        heading: 'Products'
+        heading: 'Products',
+        data: {}, // this is data object
+        isDataLoaded: false
+    }
+
+    fetchProudcts = () => {
+        fetch('https://dummyjson.com/products')
+            .then(res => res.json())
+            .then(results => {
+                // console.log(results);
+                console.log('updating the products state')
+                this.setState({
+                    data: results, // updating the state
+                    // isDataLoaded: true
+                })
+            })
     }
     //is invoked immediately after a component is mounted (inserted into the tree)
     componentDidMount = () => {
-        console.log('componetDidMount')
+        console.log('calling componetDidMount')
+        this.fetchProudcts();
         //normal function
 
         // window.setTimeout(function(){
@@ -18,62 +34,44 @@ class Products extends Component {
         //arrow function
         window.setTimeout(() => {
             this.setState({
-                heading: "Products List"
+                heading: "Products List",
+                isDataLoaded: true
             })
         }, 10000)
     }
-    
+
+    getCardHtml = () => {
+        const products = this.state.data.products;
+        console.log('getCardHtml', products)
+        if (products && products.length > 0) {
+            return products.map((p, index) => {
+                const { title, brand, description, images, id } = p;
+                return (
+                    <Card key={index} style={{ width: '16rem', margin: "12px" }}  >
+                        <Card.Img variant="top" src={images[0]} />
+                        <Card.Body>
+                            {/* <Card.Title>{brand}</Card.Title> */}
+                            <Card.Title>{title}</Card.Title>
+                            <Card.Text>
+                                {description}
+                            </Card.Text>
+                            <Button variant="primary">ADD TO CART</Button>
+                        </Card.Body>
+                    </Card>
+                )
+            })
+        }
+    }
+    getHeading = () => {
+        return  <h1>{this.state.heading}</h1>
+    }
+
     render() {
-        console.log('render')
+        console.log('calling render', this.state)
         return (
             <div className='row'>
-
-                <h1>{this.state.heading}</h1>
-
-                <Card style={{ width: '16rem', margin: "12px" }}  >
-                    <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_185c7d750ff%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_185c7d750ff%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22106.6640625%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '16rem', margin: "12px" }} >
-                    <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_185c7d750ff%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_185c7d750ff%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22106.6640625%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '16rem', margin: "12px" }} >
-                    <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_185c7d750ff%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_185c7d750ff%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22106.6640625%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '16rem', margin: "12px" }} >
-                    <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_185c7d750ff%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_185c7d750ff%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22106.6640625%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                {this.state.isDataLoaded === true ? this.getHeading() : ''}
+                {this.getCardHtml()}
             </div>
         )
     }
